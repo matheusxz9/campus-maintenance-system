9e9c31dab4fdaeacc351a6e393a07eaa4cc0a794
Matheus William <matheuswilliambr987@gmail.com>
2026-07-09 15:32:13 -0300

    feat: modulo Chamados com CRUD, state machine e comentarios

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoriasModule } from './categorias/categorias.module';
 import { LocaisModule } from './locais/locais.module';
+import { ChamadosModule } from './chamados/chamados.module';
 
 @Module({
-  imports: [CategoriasModule, LocaisModule],
+  imports: [CategoriasModule, LocaisModule, ChamadosModule],
   controllers: [AppController],
   providers: [AppService],
 })
