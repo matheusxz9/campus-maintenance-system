65a5522fbc7a35401cd3c24c4de688f080843523
Matheus William <matheuswilliambr987@gmail.com>
2026-07-09 15:29:15 -0300

    config: estrutura basica do projeto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TarefasModule } from './tarefas/tarefas.module';
-import { ChamadosModule } from './chamados/chamados.module';
 
 @Module({
-  imports: [TarefasModule, ChamadosModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
